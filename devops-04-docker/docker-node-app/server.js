fec775a44df01dec148e967022de5a7469040379
CarlsonBuma <75643791+CarlsonBuma@users.noreply.github.com>
2026-05-13 14:33:09 +0200

    node-app

--- devops-04-docker/docker-node-app/server.js
+++ devops-04-docker/docker-node-app/server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World!!! :-) Azure Test with Docker and GH action!');
+  res.send('Nodeapp by Carlson Patrick!');
 });
 
 app.listen(PORT, HOST);
